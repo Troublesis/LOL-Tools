c7b6d6fa82fda89fd5faa78042793a93f1adcbfa
weihaohu <bamboo5320@gmail.com>
2020-12-14 10:22:32 +1100

    2020-12-09, Wed 16:59 intial release 2020-12-14, Mon 09:08 first edit 2020-12-14, Mon 10:21 first working version for champion info

--- champion-info.py
+++ champion-info.py
@@ -2,52 +2,6 @@ import requests
 import urllib.request as request
 from lxml import html
 import json
-# import datetime
-# from datetime import date
-# from datetime import timedelta
-#
-# today = date.today()
-#
-# main_url = "http://ddragon.leagueoflegends.com/cdn/10.18.1/data/en_US/champion.json"
-# # response = requests.get(main_url)
-# # champion_info_tree = html.fromstring(response.content)
-# # # print(champion_info_tree.text)
-# # champion_info_list = champion_info_tree.text
-# # # print(champion_info_list)
-# # champion_data = json.loads(champion_info_list)
-# #
-# # champion_data_file = json.dumps(champion_data["data"])
-# # # print(champion_data)
-# # with open('champion-data.txt', 'w') as f:
-# #     print('today is: ', today)
-# #
-# #     print('\n\n\n' + champion_data_file, file=f)
-# # f.close()
-#
-#
-#
-# # champion = input("Please input champion name:")
-# # url = "http://ddragon.leagueoflegends.com/cdn/10.18.1/data/en_US/champion/{}.json".format(champion)
-# # print(url)
-# # page = BeautifulSoup(requests.get(url).content)
-# # print(page.text)
-#
-# #
-# # url = "http://ddragon.leagueoflegends.com/cdn/10.18.1/data/en_US/champion/Amumu.json"
-# # response = requests.get(url)
-# # champion_detail_info_tree = html.fromstring(response.content)
-# # # # print(champion_info_tree.text)
-# # champion_detail_info_list = champion_detail_info_tree.text
-# # with open('champion-detail-data.json', 'w') as f:
-# #     json.dump(champion_detail_info_list, f)
-# # f.close()
-# #
-# #
-# # with open('champion-detail-data.json', 'r') as f:
-# #     champion_detail_info_dict = json.load(f)
-# # f.close()
-# # print(champion_detail_info_dict)
-
 
 version_url = "https://ddragon.leagueoflegends.com/api/versions.json"
 response = requests.get(version_url)
@@ -64,16 +18,7 @@ print("current version is: " + current_version)
 
 champion_list_url = "http://ddragon.leagueoflegends.com/cdn/" + current_version + "/data/en_US/champion.json"
 print(champion_list_url)
-# response = requests.get(champion_list_url)
-# champions_dict = html.fromstring(response.content).text
-# champions_dict = json.loads(champions_dict)
-# champions_dict = champions_dict["data"]
-# # print(champions_dict)
-#
-# def getList(dict):
-#     return list(dict.keys())
-#
-# champion_list = getList(champions_dict)
+
 with request.urlopen(champion_list_url) as response:
     source = response.read()
     champion_dict = json.loads(source)
@@ -89,19 +34,34 @@ print(champion_list)
 champion_name = input("please input champion name: ").capitalize()
 champion_dict_url = "http://ddragon.leagueoflegends.com/cdn/" + current_version + "/data/en_US/champion/" + champion_name + ".json"
 print(champion_dict_url)
-# response = requests.get(champion_dict_url)
-# champion_dict = html.fromstring(response.content).text
-#
-# with open('champion-detail-data.json', 'w') as f:
-#     json.dump(champion_dict, f)
-#     f.close()
-#
-# with open('champion-detail-data.json') as json_file:
-#     champion_detail = json.load(json_file)
-# print(champion_detail)
-# print("Type:", type(champion_detail))
 
 with request.urlopen(champion_dict_url) as response:
     source = response.read()
     champion_detail = json.loads(source)
-print(champion_detail)
+champion_data = champion_detail["data"][champion_name]
+
+champion_stats = champion_detail["data"][champion_name]["stats"]
+# print(champion_stats)
+
+champion_spells = champion_detail["data"][champion_name]["spells"]
+print("===Champion Info===\n")
+# print("\n" + str(champion_stats) + "\n")
+print(                                   champion_spells[0]["id"])
+print("Spell cooldown per level: " + str(champion_spells[0]["cooldown"]))
+print("Spell cost per level    : " + str(champion_spells[0]["costBurn"]))
+print("Spell range per level   : " + str(champion_spells[0]["range"]) + "\n")
+# print("\n")
+print(                                   champion_spells[1]["id"])
+print("Spell cooldown per level: " + str(champion_spells[1]["cooldown"]))
+print("Spell cost per level    : " + str(champion_spells[1]["costBurn"]))
+print("Spell range per level   : " + str(champion_spells[1]["range"]) + "\n")
+# print("\n")
+print(                                   champion_spells[2]["id"])
+print("Spell cooldown per level: " + str(champion_spells[2]["cooldown"]))
+print("Spell cost per level    : " + str(champion_spells[2]["costBurn"]))
+print("Spell range per level   : " + str(champion_spells[2]["range"]) + "\n")
+# print("\n")
+print(                                   champion_spells[3]["id"])
+print("Spell cooldown per level: " + str(champion_spells[3]["cooldown"]))
+print("Spell cost per level    : " + str(champion_spells[3]["costBurn"]))
+print("Spell range per level   : " + str(champion_spells[3]["range"]) + "\n")
