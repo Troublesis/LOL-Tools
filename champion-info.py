31c68aa1d3ae9a22ef2b660dd97a346b0ad276bb
weihaohu <bamboo5320@gmail.com>
2020-12-14 11:06:08 +1100

    2020-12-09, Wed 16:59 intial release 2020-12-14, Mon 09:08 first edit 2020-12-14, Mon 10:21 first working version for champion info 2020-12-14, Mon 10:34 cleaned up the champion info code, added a loop 2020-12-14, Mon 11:04 champion-info  ====> make the code easier to understand

--- champion-info.py
+++ champion-info.py
@@ -1,39 +1,37 @@
-import requests
 import urllib.request as request
-from lxml import html
 import json
-def champion_info():
-    version_url = "https://ddragon.leagueoflegends.com/api/versions.json"
-    response = requests.get(version_url)
-    version_list = html.fromstring(response.content)
-    version_list = version_list.text
-    # print(version_list)
-    # convert json into list
-    version_list = json.loads(version_list)
-    # print(type(version_list))
-    # print(version_list)
-    # get current version
-    current_version = version_list[0]
-    print("current version is: " + current_version)
 
+def champion_info():
+    # setup initial lol version check url
+    version_url = "https://ddragon.leagueoflegends.com/api/versions.json"
+    with request.urlopen(version_url) as response:
+        source = response.read()
+        version_list = json.loads(source)
+    # get latest version number
+    current_version = version_list[0]
+
+
+    # get champion list
     champion_list_url = "http://ddragon.leagueoflegends.com/cdn/" + current_version + "/data/en_US/champion.json"
     print(champion_list_url)
-
     with request.urlopen(champion_list_url) as response:
         source = response.read()
         champion_dict = json.loads(source)
     champion_dict = champion_dict["data"]
     # print(champion_dict)
 
+    # return dict key value
     def getList(dict):
         return list(dict.keys())
 
     champion_list = getList(champion_dict)
     print(champion_list)
 
-    champion_name = input("please input champion name: ").capitalize()
+    print("\nCurrent League of Legends version is: " + current_version)
+    # get champion info
+    champion_name = input("Please input champion name: ").capitalize()
     champion_dict_url = "http://ddragon.leagueoflegends.com/cdn/" + current_version + "/data/en_US/champion/" + champion_name + ".json"
-    print(champion_dict_url)
+    # print(champion_dict_url)
 
     with request.urlopen(champion_dict_url) as response:
         source = response.read()
@@ -47,8 +45,10 @@ def champion_info():
     print("===Champion Info===\n")
     # todo: add champion stats info, also check if more level info need to be added
     # print("\n" + str(champion_stats) + "\n")
-    for spell in range(3):
-        print(                                   str(champion_spells[spell]["id"]) + ": " + str(champion_spells[spell]["name"]))
+
+    spell_list = ["Q", "W", "E", "R"]
+    for spell in range(4):
+        print(spell_list[spell] + ": " + str(champion_spells[spell]["id"]) + " - " + str(champion_spells[spell]["name"]))
         print("Spell cooldown per level: " + str(champion_spells[spell]["cooldown"]))
         print("Spell cost per level    : " + str(champion_spells[spell]["costBurn"]))
         print("Spell range per level   : " + str(champion_spells[spell]["range"]) + "\n")
